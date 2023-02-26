eee61d533b76c51d7a409438833ba4eb7dbaac1b
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-02-26 14:09:12 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 
 st.title(":red[MY PROFILE]")
+st.subheader(":red[PEACE ODUOBUK EDWARD]")
 st.markdown(
     """
     <style>
@@ -20,6 +21,6 @@ st.markdown(
       -Github: (https://github.com/PeaceEdward)
     '''
 )
-st.subheader(":red[PEACE ODUOBUK EDWARD]")
+
 st.caption(" My name is Peace Edward and I'm thrilled to introduce myself as a student pharmacist and practicing data scientist. I have always been passionate about healthcare and technology,and I believe that the combination of these two fields has the potential to bring about significant positive changes in the world.")
 st.caption("As a student pharmacist, I have gained a solid foundation in the clinical sciences, pharmacology, and patient care. On the other hand, as a practicing data scientist, I have developed skills in data analysis, machine learning, and data visualization. I'm always eager to learn and to expand my knowledge in both of these areas.")
