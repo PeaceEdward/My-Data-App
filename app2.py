79bd9ebc98c6ea0c579e8f685a31358da3db985d
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-02-26 14:04:58 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -14,6 +14,12 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+st.markdown(
+    '''
+      -LinkedIn: (https://www.linkedin.com/in/peace-edward-5917b322a/)
+      -Github: (github.com/PeaceEdward)
+    '''
+)
 st.subheader(":red[PEACE ODUOBUK EDWARD]")
 st.caption(" My name is Peace Edward and I'm thrilled to introduce myself as a student pharmacist and practicing data scientist. I have always been passionate about healthcare and technology,and I believe that the combination of these two fields has the potential to bring about significant positive changes in the world.")
 st.caption("As a student pharmacist, I have gained a solid foundation in the clinical sciences, pharmacology, and patient care. On the other hand, as a practicing data scientist, I have developed skills in data analysis, machine learning, and data visualization. I'm always eager to learn and to expand my knowledge in both of these areas.")
