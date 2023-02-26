f5cda810e37ca2145dcbcc746c1b0d8b04b2b6c3
PeaceEdward <104164613+PeaceEdward@users.noreply.github.com>
2023-02-26 22:47:27 +0100

    Update Titanic.py

--- pages/Titanic.py
+++ pages/Titanic.py
@@ -4,41 +4,34 @@ import pandas as pd
 import plotly.express as px
 import os  
 
-
+#using os to get file path of image and dataset
 FILE_DR=os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR=os.path.join(FILE_DR,os.path.pardir)
-
 dir_of_interest=os.path.join(PARENT_DIR,"resources")
-
 IMAGE_PATH=os.path.join(dir_of_interest,"images","titanic.jpg")
 DATA_PATH=os.path.join(dir_of_interest,"data","titanic.csv")
 
+#reading in image and data
 img=image.imread(IMAGE_PATH)
 df=pd.read_csv(DATA_PATH)
+
 st.header(':red[TITANIC ANALYSIS DASHBOARD]')
+#rendering image to streamlit
 st.image(img)
 
 st.subheader('Filter the dataset by gender')
-#st.dataframe(df)
 
-#create a slider to show just a couple of ages
-#df=df.dropna(inplace=True)
-#df=df["age"]=df["age"].astype(int)
-#min_value=df['age'].min()
-#max_value=df['age'].max()
-
-#value= st.slider("Select value range",min_value,max_value)
-#filtered_data=df[df['age']==value]
-#st.dataframe(filtered_data)
-
+#creating a selectbox to filter dataset by gender
 sex=st.selectbox("Select gender",df['sex'].unique())
 filtered_df=df[df['sex']==sex]
 st.dataframe(filtered_df)
 
+#creating histogram to show surviving passengers per city
 col1,col2=st.columns(2)
 fig1=px.histogram(df[df['alive']=='yes'],x='embark_town',title='Count of surviving passengers per city')
 col1.plotly_chart(fig1,use_container_width=True)
 
+#creating a pie chart to show passengers per class
 import plotly.graph_objs as go
 #aggregate dataframe to get count of people in each clas
 agg_df=df.groupby('class')['class'].count()
